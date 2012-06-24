e0646cec87c19c641c6a0a51099770ad05063b12
yama@muteki <yama@muteki.com>
2012-06-24 17:11:41 +0900

    タイムゾーンをTokyoに設定。

--- yama-muteki-test1/Default.aspx.cs
+++ yama-muteki-test1/Default.aspx.cs
@@ -9,19 +9,21 @@ namespace WebApplication4
 {
     public partial class Default : System.Web.UI.Page
     {
+        private TimeZoneInfo tokyo = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            this.Label1.Text = DateTime.Now.ToString();
+            this.Label1.Text = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tokyo).ToString();
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            this.Label1.Text = DateTime.Now.ToString();
+            this.Label1.Text = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tokyo).ToString();
         }
 
         protected void Button2_Click(object sender, EventArgs e)
         {
-            this.Label1.Text = DateTime.Now.ToString("yyyy/MM/dd");
+            this.Label1.Text = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tokyo).ToString("yyyy/MM/dd");
         }
     }
 }
